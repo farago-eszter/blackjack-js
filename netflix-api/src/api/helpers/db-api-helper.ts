a3770fb2da6f8e266b1e60303a43a8e8ece51d98
farago-eszter <farago.eszter02@gmail.com>
2026-08-12 22:10:02 +0200

    Lesson 7

--- netflix-api/src/api/helpers/db-api-helper.ts
+++ netflix-api/src/api/helpers/db-api-helper.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const dbApiClient = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "http://netflix-db-api:4000/api/v1",
 });
 
 export function mapObjectFromDb(mongoObject: any) {
